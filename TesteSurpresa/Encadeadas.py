b8ae5788e5170c802881d1093471ec4eb4328690
KEVIN DA SILVA SOUZA <631810004@fspoaeduc.com.br>
2018-10-24 21:01:13 -0300

    Commit com teste melhorado

--- TesteSurpresa/Encadeadas.py
+++ TesteSurpresa/Encadeadas.py
@@ -15,6 +15,17 @@ class Lista:
         """Inicializa uma nova lista."""
         self.head = None
         self.tail = None
+        self.size = 0
+
+    def _executaWhile(self, i):
+        while i.proximo is not None:
+            anterior = i
+            i = i.proximo
+            if i.proximo is None:
+                valor = i.dado
+                anterior.proximo = None
+                self.tail = anterior
+        return valor
 
     def append(self, valor):
         """Adiciona um valor ao final da lista."""
@@ -23,16 +34,19 @@ class Lista:
         else:
             self.tail.proximo = No(valor)
             self.tail = self.tail.proximo
+        self.size += 1
 
     def addFirst(self, valor):
-        if self.head is not None:
+        # if self.head is not None:
             real_head = self.head
             self.head = No(valor)
             self.head.proximo = real_head
+            self.size += 1
 
     def removeFirst(self):
         if self.head is not None:
             self.head = self.head.proximo
+            self.size -= 1
 
     def removeLast(self):
         if self.head is not None:
@@ -41,25 +55,11 @@ class Lista:
             if self.head.proximo is None:
                 self.head = self.proximo = None
             else:
-                while i.proximo is not None:
-                    # print(i.dado)
-                    anterior = i
-                    i = i.proximo
-                    if i.proximo is None:
-                        # print'Dado:', anterior.dado
-                        anterior.proximo = None
-                        self.tail = anterior
+                self._executaWhile(i)
+            self.size -= 1
 
-    def size(self):
-        if self.head is None:
-            i = 0
-        else:
-            i = 1
-            valor = self.head
-            while valor.proximo is not None:
-                i += 1
-                valor = valor.proximo
-        print("Size", i)
+    def listSize(self):
+        return (self.size)
 
     def remove(self, x):
         if self.head is not None:
@@ -76,24 +76,19 @@ class Lista:
                 if i.dado == x:
                     anterior.proximo = i.proximo
                     i = anterior
+                    self.size -= 1
 
     def pop(self):
-        i = self.head
-        anterior = None
-        valor = None
-        if self.head.proximo is None:
-            self.head = self.proximo = None
-        else:
-            while i.proximo is not None:
-                # print(i.dado)
-                anterior = i
-                i = i.proximo
-                if i.proximo is None:
-                    # print'Dado:', anterior.dado
-                    valor = i.dado
-                    anterior.proximo = None
-                    self.tail = anterior
-        return (valor)
+        if self.head is not None:
+            i = self.head
+            anterior = None
+            valor = None
+            if self.head.proximo is None:
+                self.head = self.proximo = None
+            else:
+                valor = self._executaWhile(i)
+            self.size -= 1
+            return (valor)
 
     def first(self):
         if self.head is not None:
@@ -124,8 +119,8 @@ x.removeFirst()
 x.removeLast()
 print("First:", x.first().dado)
 print("Last:", x.last().dado)
+print("Size:", x.listSize())
 i = x.head
-x.size()
 while i is not None:
     print(i.dado)
     i = i.proximo
